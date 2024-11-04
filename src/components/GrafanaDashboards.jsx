62ae74961c3d84d69253526de383137a3f21b0f8
Pratham Jain <97790260+Pratham-Jain-3903@users.noreply.github.com>
2024-11-05 00:51:26 +0530

    Update GrafanaDashboards.jsx

--- src/components/GrafanaDashboards.jsx
+++ src/components/GrafanaDashboards.jsx
@@ -27,6 +27,7 @@ const GrafanaDashboard = ({ dashboardId }) => {
       <h1>{dashboard.dashboard.title}</h1>
       <iframe
 {/*         src={`http://your-grafana-url/d/${dashboardId}`} */}
+        src = {'https://krishnafauj02.grafana.net/public-dashboards/9942ae56688b4a73b89370d6040bb2d6'}
         width="100%"
         height="600"
         frameBorder="0"
